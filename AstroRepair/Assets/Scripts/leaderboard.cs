f3787719fef2212f682483a2326dc682ced43f7e
Lucas <lucas.huet-de-guerville@epitech.eu>
2020-02-01 12:14:28 +0100

    start add script to modify a text

---
+++ AstroRepair/Assets/Scripts/leaderboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class leaderboard : MonoBehaviour
+{
+    public Text mytext;
+
+    string str = "hello there";
+
+    void Start()
+    {
+        mytext.text = str;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
