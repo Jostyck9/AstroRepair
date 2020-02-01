28096e5e7c50fc6f9d7856d6af9d1143b262413c
Lucas <lucas.huet-de-guerville@epitech.eu>
2020-02-01 10:01:34 +0100

    methods in a script for main menu buttons, matter with change scene because i didnt add the new scenes to the build

---
+++ AstroRepair/Assets/ButtonHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
