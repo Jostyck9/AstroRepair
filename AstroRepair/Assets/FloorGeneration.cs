8cc7e39ba2fb8c9cf149f1c2f75014325705b108
CasimiroJ <jules.casimiro@epitech.eu>
2020-02-01 12:09:12 +0100

    Add assets

---
+++ AstroRepair/Assets/FloorGeneration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FloorGeneration : MonoBehaviour
+{
+    public TileBase tileBase;
+    public int maxX;
+    public int maxY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < maxX; i++)
+        {
+            for (int j = 0; j < maxY; j++)
+            {
+                this.GetComponent<Tilemap>().SetTile(new Vector3Int(i, j, 0), tileBase);
+            }
+        }
+        this.GetComponent<Tilemap>().RefreshAllTiles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AstroRepair/Assets/MapGeneration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGeneration : MonoBehaviour
+{
+    public TileBase tileBase;
+    public int maxX;
+    public int maxY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = Random.Range(0, 50);
+        for (int i = 0; i < max; i++)
+        {
+            int x = Random.Range(0, maxX);
+            int y = Random.Range(0, maxY);
+            this.GetComponent<Tilemap>().SetTile(new Vector3Int(x, y, 0), tileBase);
+        }
+        this.GetComponent<Tilemap>().RefreshAllTiles();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
