13aee1da96575fc0dc8e92e0211e6956a03b551e
Lucas <lucas.huet-de-guerville@epitech.eu>
2020-02-01 16:14:55 +0100

    can read from the json

--- AstroRepair/Assets/Scripts/leaderboard.cs
+++ AstroRepair/Assets/Scripts/leaderboard.cs
@@ -2,20 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
-public class leaderboard : MonoBehaviour
+public class Leaderboard : MonoBehaviour
 {
-    public Text mytext;
-
-    string str = "hello there";
+    string path, jsonString;
 
     void Start()
     {
-        mytext.text = str;
+        path = Application.streamingAssetsPath + "/leaderboard.json";
+        jsonString = File.ReadAllText(path);
+
+        Players myList = JsonUtility.FromJson<Players>(jsonString);
+        foreach (Player player in myList.players)
+        {
+            Debug.Log("name: " + player.name + " time: " + player.time + " pieces: " + player.pieces);
+        }
     }
     // Update is called once per frame
     void Update()
     {
         
     }
+}
+
+[System.Serializable]
+public class Player
+{
+    public string name;
+    public string time;
+    public int pieces;
+}
+
+[System.Serializable]
+public class Players
+{
+    public Player[] players;
 }
