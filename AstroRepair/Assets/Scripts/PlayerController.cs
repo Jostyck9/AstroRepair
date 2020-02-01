fc4278554d11f000728e170c667d24f453a1f00f
Jostyck9 <hugo.berthome@epitech.eu>
2020-02-01 01:48:21 +0100

    adding moving characters

---
+++ AstroRepair/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float coefSprint;
+    public Rigidbody2D rb;
+
+    private bool isRunning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (Input.GetAxis("Sprint") > 0)
+            isRunning = true;
+        else
+            isRunning = false;
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        if (isRunning)
+            rb.velocity = (movement * (speed * coefSprint) * Time.deltaTime);
+        else
+            rb.velocity = (movement * speed * Time.deltaTime);
+    }
+}
