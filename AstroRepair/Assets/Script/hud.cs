3b03d7f26e2680813fc362a2b7640f5ce537a9fd
Jean Delaunay <jean.delaunay@epitech.eu>
2020-02-02 00:40:31 +0100

    a

---
+++ AstroRepair/Assets/Script/hud.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hud : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float _currentO2 = 100;
+    public float _maxO2 = 100;
+    public float _currentObject= 0;
+    public float _maxObject = 5;
+    public Slider _slider;
+    public Text _object;
+    public Text _porcent;
+    public float _speed = 0.01f;
+    void Start()
+    {
+        
+    }
+
+    // FicedUpdate is called once per frame
+    void FixedUpdate()
+    {
+        if (_currentO2 != 0)
+        {
+            _currentO2 -= 1 * _speed;
+            float progress = Mathf.Clamp01(_currentO2 / _maxO2);
+            _slider.value = progress;
+            _porcent.text = Mathf.Round(progress * 100) + "%";
+        }
+        if (Input.GetKeyUp(KeyCode.P))
+            {
+            _currentObject += 1;
+            if (_currentObject < 2)
+            {
+                _object.text = "Object : " + _currentObject + " / 4";
+
+            }
+            if (_currentObject >= 2)
+            {
+                _object.text = "Objects : " + _currentObject + " / 5";
+            }
+        }
+    }
+}
---
+++ AstroRepair/Assets/Script/initPos.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class initPos : MonoBehaviour
+{
+    public Slider _slider;
+    public Text _textOxygen;
+    public Text _textPorcent;
+    public Text _textObjet;
+    public Text _textIndice;
+    private int _largeurEcran;
+    private int _hauteurEcran;
+    private Camera _cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _largeurEcran = Screen.width;
+        _hauteurEcran = Screen.height;
+        Vector3 size = _cam.ScreenToWorldPoint(new Vector3(_largeurEcran, _hauteurEcran,  0));
+
+        Vector3 posSlider = new Vector3(0.7f * size.x, 1.4f * size.y, -5);
+        Vector3 posTextOxygen = new Vector3(0.6f * size.x, 1.2f * size.y, -5);
+        Vector3 posTextPorcent = new Vector3(-0.40f * size.x, 1.35f * size.y, -5);
+        Vector3 posTextIndice = new Vector3(0.25f * size.x, 1.45f * size.y, -5);
+        Vector3 posTextObject = new Vector3(3.3f * size.x, -1.5f * size.y, -5);
+
+        _slider.transform.localPosition = posSlider;
+        _textOxygen.transform.localPosition = posTextOxygen;
+        _textIndice.transform.localPosition = posTextIndice;
+        _textPorcent.transform.localPosition = posTextPorcent;
+        _textObjet.transform.localPosition = posTextObject;
+        _slider.transform.localScale = new Vector3(0.025f * size.x, 0.04f * size.y, 1);
+        _textOxygen.transform.localScale = new Vector3(0.04f * size.x, 0.04f * size.y, 1);
+        _textIndice.transform.localScale = new Vector3(0.03f * size.x, 0.03f * size.y, 1);
+        _textPorcent.transform.localScale = new Vector3(0.01f * size.x, 0.01f * size.y, 1);
+        _textObjet.transform.localScale = new Vector3(0.015f * size.x, 0.015f * size.y, 1);
+    }
+}
