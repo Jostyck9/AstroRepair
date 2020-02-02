6c2ea67202ce3a3a3cf05afbcd6bb00318310a93
Lucas <lucas.huet-de-guerville@epitech.eu>
2020-02-02 12:52:56 +0100

    add text score in defeat/victory and start the script to display it

---
+++ AstroRepair/Assets/Scripts/ScoreEndMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreEndMenu : MonoBehaviour
+{
+    public Text textScore;
+    string time, pieces, display;
+    void Start()
+    {
+        /*        time = getTime().toString();*/
+        /*        pieces = getPieces().soString();*/
+        display = "Final score:\n" + time + " seconds for a score of: " + pieces;
+    }
+
+    void Update()
+    {
+        textScore.text = display;
+    }
+}
