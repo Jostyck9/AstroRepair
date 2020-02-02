6ee21ac6a472546d47d477f1c82d0aba902648ff
Jostyck9 <hugo.berthome@epitech.eu>
2020-02-02 12:16:19 +0100

    adding score scipt

---
+++ AstroRepair/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public float time = 0;
+    public static float lastTime = 0;
+
+    public uint pieces = 0;
+    public static uint lastPieces = 0;
+
+    public bool startTimeScore = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTimeScore)
+        {
+            time += Time.deltaTime;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        lastTime = time;
+        lastPieces = pieces;
+    }
+
+    public uint getLastPieces()
+    {
+        return lastPieces;
+    }
+
+    public float getLastTime()
+    {
+        return lastTime;
+    }
+
+    public void startScore()
+    {
+        startTimeScore = true;
+    }
+}
